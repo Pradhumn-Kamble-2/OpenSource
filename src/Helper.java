8dbad5b461cbb1220075020ce3eedb2ca3cc5e9d
Kamble, Pradhumn <pradhumn.kamble@ansira.com>
2024-11-03 19:02:28 +0530

    Update Helper class

--- src/Helper.java
+++ src/Helper.java
@@ -1,5 +1,21 @@
+class A{
+    int x ;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+}
+
 public class Helper{
     public static void main(){
         System.out.println("Helper");
+        A a = new A();
+        a.setX(10);
+        System.out.println(a.getX());
+
     }
 }
